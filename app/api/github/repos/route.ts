f953f504136dfef75e6d7c4b4d495667d04ae5bf
barryanh <anhtruong20001999@gmail.com>
2026-07-13 21:13:27 +0700

    Fetch Users Github Repo List

---
+++ app/api/github/repos/route.ts
@@ -0,0 +1,49 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("gh_token")?.value;
+
+  if (!token) {
+    return NextResponse.json(
+      JSON.stringify({ error: "Không tìm thấy token GitHub!" }),
+      { status: 401 },
+    );
+  }
+
+  const allRespo = [];
+  let page = 1;
+
+  while (true) {
+    const res = await fetch(
+      `https://api.github.com/user/repos?per_page=100&page=${page}&sort=updated`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/vnd.github+json",
+        },
+      },
+    );
+
+    const respos = await res.json();
+    if (!respos.length) break;
+    allRespo.push(...respos);
+    page++;
+  }
+
+  return NextResponse.json(
+    allRespo.map((r) => ({
+      id: r.id,
+      name: r.name,
+      full_name: r.full_name,
+      private_: r.private,
+      html_url: r.html_url,
+      description: r.description,
+      updated_at: r.updated_at,
+      language: r.language,
+      default_branch: r.default_branch,
+      owner: r.owner.login,
+    })),
+  );
+}
